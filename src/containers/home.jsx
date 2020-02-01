f8ae167a8fa77c8a4764f470727dfd3ff69e2923
Redeemer40 <victorjonah199@gmail.com>
2020-02-01 10:31:19 +0100

    Homepage

---
+++ src/containers/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Carousel from "../components/carousel";
+import Card from "../components/card";
+
+const Home = () => {
+  return (
+    <div>
+      <Carousel />
+      <Card />
+    </div>
+  );
+};
+
+export default Home;
