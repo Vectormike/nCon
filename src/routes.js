4afd3cd110b6934de919c2a33e8faa834c500c1b
Redeemer40 <victorjonah199@gmail.com>
2020-02-01 03:15:58 +0100

    ...

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+const Router = () => {
+  return (
+    <div>
+      <Switch></Switch>
+    </div>
+  );
+};
+
+export default Router;
