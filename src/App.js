e045b16d95341df4e5edf0212ff315703a34c222
Redeemer40 <victorjonah199@gmail.com>
2020-02-01 10:32:59 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import { Route } from "react-router-dom";
 import Header from "./components/header";
 import Footer from "./components/footer";
 
+import Routes from "./routes";
+
 import "./App.css";
+import Home from "./containers/home";
 
 function App() {
   return (
     <div className="App">
       <Route component={Header} />
+      <Route component={Home} />
       <Route component={Footer} />
     </div>
   );
