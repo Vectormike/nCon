085d2e870a0403ea2c90f52206fdfba12d5d4e12
Redeemer40 <victorjonah199@gmail.com>
2020-02-02 04:16:02 +0100

    ...

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,14 +2,19 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div className="card mb-3" style={{ maxWidth: "540px" }}>
+    <div
+      className="card mb-3"
+      style={({ maxWidth: "1020px" }, { padding: "5px" }, { margin: "10px" })}
+    >
       <div className="row no-gutters">
-        <div className="col-md-4">
+        <div className="col-md-6">
           <img src="..." className="card-img" alt="..." />
         </div>
-        <div className="col-md-8">
+        <div className="col-md-6">
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title">
+              Welcome to Computer Science University of Uyo
+            </h5>
             <p className="card-text">
               This is a wider card with supporting text below as a natural
               lead-in to additional content. This content is a little bit
