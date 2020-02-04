39c4e6b89e2a0d95d44365d7d8b933db85f842f6
Redeemer40 <victorjonah199@gmail.com>
2020-02-04 15:05:28 +0100

    ft: news page

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./containers/home";
+import News from "./containers/news";
 
 const Router = () => {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/news" component={News} />
       </Switch>
     </div>
   );
