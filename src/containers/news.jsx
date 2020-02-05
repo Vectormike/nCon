70fb6f2b41c85d73d5f8c4f6d026d9c1c475a33e
Redeemer40 <victorjonah199@gmail.com>
2020-02-05 17:23:50 +0100

    ...

--- src/containers/news.jsx
+++ src/containers/news.jsx
@@ -1,10 +1,31 @@
 import React from "react";
-import Posts from "../helpers/custom-hooks/usePosts";
+import { Link } from "react-router-dom";
+import usePosts from "../helpers/custom-hooks/usePosts";
 
 const News = () => {
+  const [posts, isLoading] = usePosts();
+
+  const renderPosts = () => {
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    return posts.map(post => (
+      <Link to={post.fields.slug}>
+        <div>
+          <img src={post.fields.featuredImage.fields.fields} />
+        </div>
+        <h3>{post.fields.title}</h3>
+        <p>{post.fields.description}</p>
+      </Link>
+    ));
+  };
+
   return (
-    <div>
-      <Posts />
+    <div className="posts__container">
+      <h2>Articles</h2>
+
+      <div className="posts">{renderPosts()}</div>
     </div>
   );
 };
