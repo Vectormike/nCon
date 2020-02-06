817ada9c0b0238e231fa146f039994aadbb264a1
Redeemer40 <victorjonah199@gmail.com>
2020-02-06 21:22:09 +0100

    ...

--- src/containers/news.jsx
+++ src/containers/news.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import usePosts from "../helpers/custom-hooks/usePosts";
+import { useNews } from "../helpers/custom-hooks/index";
+import "../styles/news.css";
 
 const News = () => {
-  const [posts, isLoading] = usePosts();
+  const [posts, isLoading] = useNews();
 
   const renderPosts = () => {
-    if (isLoading) {
-      return <p>Loading...</p>;
-    }
+    if (isLoading) return <p>Loading...</p>;
 
     return posts.map(post => (
-      <Link to={post.fields.slug}>
-        <div>
-          <img src={post.fields.featuredImage.fields.fields} />
+      <Link className="posts__post" to={post.fields.slug}>
+        <div className="posts__post__img__container">
+          <img
+            className="posts__post__img__container__img"
+            alt={post.fields.title}
+            src={post.fields.featuredImage.fields.fields}
+          />
         </div>
+        <small>{post.fields.date}</small>
         <h3>{post.fields.title}</h3>
         <p>{post.fields.description}</p>
       </Link>
@@ -23,7 +27,7 @@ const News = () => {
 
   return (
     <div className="posts__container">
-      <h2>Articles</h2>
+      <h2>News</h2>
 
       <div className="posts">{renderPosts()}</div>
     </div>
