c75cd134a77699bd373edea6c0d331a611361487
Redeemer40 <victorjonah199@gmail.com>
2020-02-09 15:39:53 +0100

    ...

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./containers/home";
 import News from "./containers/news";
+import SingleNews from "./containers/singleNews";
+import Payment from "./containers/payment";
 
 const Router = () => {
   return (
@@ -9,6 +11,8 @@ const Router = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/news" component={News} />
+        <Route path="/news/:id" component={SingleNews} />
+        <Route path="/payment" component={Payment} />
       </Switch>
     </div>
   );
