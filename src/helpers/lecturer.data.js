8643bb882dd2d3bc0eaee8afcd31f00e1863a37d
Redeemer40 <victorjonah199@gmail.com>
2020-02-19 23:09:17 +0100

    ...

--- src/helpers/lecturer.data.js
+++ src/helpers/lecturer.data.js
@@ -3,35 +3,35 @@ const LECTURER_DATA = [
     id: 1,
     name: "Dr (Mrs) Uduak Umoh",
     status: "Head of Department",
-    qualification: "Ph.D, M.Sc(UNIPORT), B.Sc(UNIUYO)",
-    speciality: "Soft Computing and database Management Systems"
+    qualification: "Ph.D, M.Sc (UNIPORT), B.Sc( UNIUYO)",
+    speciality: "Soft Computing and Database Management Systems"
   },
   {
     id: 2,
     name: "Dr Gabriel Udoinyang",
     status: "Lecturer 1",
-    qualification: "Ph.d, M.Tech. (FUTA), B.Sc(UNIUYO)",
+    qualification: "Ph.D, M.Tech. (FUTA), B.Sc (UNIUYO)",
     speciality: "Soft Computing and Knowledge Engineering"
   },
   {
     id: 3,
     name: "Dr. Moses E. Ekpenyong",
     status: "Lecturer 1",
-    qualification: "Ph.D(UNIUYO), M.Sc(ATBU), B.Sc(UNIUYO)",
+    qualification: "Ph.D (UNIUYO), M.Sc (ATBU), B.Sc (UNIUYO)",
     speciality: "Speech and Wireless Communication"
   },
   {
     id: 4,
     name: "Dr. Okure U. Obot",
     status: "Lecturer 1",
-    qualification: "Ph.D, M.Tech(FUTA), B.Sc(UNIPORT)",
+    qualification: "Ph.D, M.Tech (FUTA), B.Sc (UNIPORT)",
     speciality: "Soft Computing and Knowledge Engineering"
   },
   {
     id: 5,
     name: "Dr. Victor E. Ekong",
     status: "Lecturer 1",
-    qualification: "Ph.D, M.Tech(UNIBEN), B.Sc(UNIUYO)",
+    qualification: "Ph.D, M.Tech (UNIBEN), B.Sc (UNIUYO)",
     speciality:
       "Software Engineering, Soft Computing, Medical Informatics and Information Technology"
   },
@@ -39,14 +39,14 @@ const LECTURER_DATA = [
     id: 6,
     name: "Dr. Uyinomen Ekong",
     status: "Senior Lecturer",
-    qualification: "Ph.D(UNIBEN), M.Sc(Covenant Uni), B.Sc(AAU)",
+    qualification: "Ph.D (UNIBEN), M.Sc (Covenant Uni), B.Sc (AAU)",
     speciality: "Cyber Security and Information Technology"
   },
   {
     id: 7,
     name: "Dr. Patience Usoro Usip",
     status: "Senior Lecturer",
-    qualification: "Ph.D(UI), M.Sc(UI), B.Sc(UNICAL)",
+    qualification: "Ph.D (UI), M.Sc (UI), B.Sc (UNICAL)",
     speciality:
       "Knowledge Representation and Reasoning (KRR), Formal Ontologies"
   },
@@ -54,35 +54,35 @@ const LECTURER_DATA = [
     id: 8,
     name: "Dr. Daniel E. Asuquo",
     status: "Lecturer 1",
-    qualification: "Ph.D(UNIPORT), M.Sc(UI), B.Sc(UNICAL)",
+    qualification: "Ph.D (UNIPORT), M.Sc (UI), B.Sc (UNICAL)",
     speciality: "Wireless Communication and Networking"
   },
   {
     id: 9,
     name: "Dr. Samuel S. Udoh",
     status: "Lecturer 1",
-    qualification: "Ph.D(FUTA), M.Sc(FUTA), B.Sc(UNIUYO)",
+    qualification: "Ph.D (FUTA), M.Sc (FUTA), B.Sc (UNIUYO)",
     speciality: "Artificial Intelligence and Remote Sensing"
   },
   {
     id: 10,
     name: "Dr. Edward N. Udoh",
     status: "Lecturer 1",
-    qualification: "Ph.D(UNIBEN), M.Sc(UNIPORT), B.Sc(UNIUYO)",
+    qualification: "Ph.D (UNIBEN), M.Sc (UNIPORT), B.Sc (UNIUYO)",
     speciality: "Software Engineering and Systems Security"
   },
   {
     id: 11,
     name: "Mrs Imo Eyoh",
     status: "Lecturer 1",
-    qualification: "Ph.D(Nottingham, UK), M.Sc(UI), B.Sc(UNIUYO)",
+    qualification: "Ph.D (Nottingham, UK), M.Sc (UI), B.Sc (UNIUYO)",
     speciality: "Intuitionistic Fuzzy Systems and Uncertainty Modeling"
   },
   {
     id: 12,
     name: "Mr Olufemi Sunday Adeoye",
     status: "Lecturer 1",
-    qualification: "M.Tech(FUTA), B.Sc(UI)",
+    qualification: "M.Tech (FUTA), B.Sc (UI)",
     speciality:
       "Data Mining, Machine Learning, Intelligent Systems and Software Engineering"
   },
@@ -90,56 +90,56 @@ const LECTURER_DATA = [
     id: 13,
     name: "Mr Uduak David George",
     status: "Lecturer 1",
-    qualification: "Ph.D(in progress) M.Tech(FUTA), B.Sc(UNIUYO)",
+    qualification: "Ph.D (in progress) M.Tech (FUTA), B.Sc (UNIUYO)",
     speciality: "Data Mining, Knowledge Discovery"
   },
   {
     id: 14,
     name: "Mr Ini John Umoeka",
     status: "Lecturer II",
-    qualification: "M.Sc(UNIPORT), B.Sc(UNIUYO)",
+    qualification: "M.Sc (UNIPORT), B.Sc (UNIUYO)",
     speciality: "Computer Systems Security"
   },
   {
     id: 15,
     name: "Mr Ifiok James Udo",
     status: "Lecturer II",
-    qualification: "M.Sc(OAU), B.Sc(UNIUYO)",
+    qualification: "M.Sc (OAU), B.Sc (UNIUYO)",
     speciality: "Information Stystem"
   },
   {
     id: 16,
     name: "Mr Samuel Akpan Robinson",
     status: "Assistant Lecturer",
-    qualification: "M.Sc(UNIBEN), B.Sc(UNIUYO)",
+    qualification: "M.Sc (UNIBEN), B.Sc (UNIUYO)",
     speciality: "Computational Intelligence"
   },
   {
     id: 17,
     name: "Mr Funebi Francis Ijebu",
     status: "Assistant Lecturer",
-    qualification: "M.C(HIT, China), B.Sc(LONDON)",
+    qualification: "M.C (HIT, China), B.Sc (LONDON)",
     speciality: "Software Engineering and Systems Security"
   },
   {
     id: 18,
     name: "Mr E. A. Dan",
     status: "Assistant Lecturer",
-    qualification: "B.Sc(UNIUYO)",
+    qualification: "B.Sc (UNIUYO)",
     speciality: ""
   },
   {
     id: 19,
     name: "Ms Mercy E. Edoho",
     status: "Assistant Lecturer",
-    qualification: "B.Sc(UNIUYO)",
+    qualification: "B.Sc (UNIUYO)",
     speciality: ""
   },
   {
     id: 20,
     name: "Mr E. E. Ekong",
     status: "",
-    qualification: "M.Sc(ATBU), PGD(OAU), B.Sc(UNILAG)",
+    qualification: "M.Sc (ATBU), PGD (OAU), B.Sc (UNILAG)",
     speciality: ""
   }
 ];
